06d6ef0442f7f42c7acd5278e8714b3efe451a9a
Jingjing Yang <jingjing.yang@tuni.fi>
2025-11-25 13:55:06 +0200

    Add second test case to check github workflow running

--- test/manual/reduce.test.js
+++ test/manual/reduce.test.js
@@ -6,4 +6,9 @@ describe("reduce.js", () => {
     const result = reduce([1, 2, 3], (sum, n) => sum + n, 0);
     expect(result).toBe(6);
   });
+
+  it("should sum array with negative numbers", () => {
+    const result = reduce([5, -3, 2], (sum, n) => sum + n, 0);
+    expect(result).toBe(4);
+  });
 });
